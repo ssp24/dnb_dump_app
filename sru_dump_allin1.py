3d1a7a732e835d46b298bb33c57382727ffe10d5
ssp24 <62755563+ssp24@users.noreply.github.com>
2025-01-31 14:51:43 +0100

    Add files via upload

--- sru_dump_allin1.py
+++ sru_dump_allin1.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, \
     QPushButton, QComboBox, QProgressBar, QSpacerItem, QSizePolicy
 from PyQt5.QtCore import Qt, QThread, pyqtSignal
+from PyQt5.QtGui import QPixmap
 from datetime import date
 #from sru_functions import dnb_sru, dnb_sru_number
 import requests
@@ -182,7 +183,10 @@ def dnb_sru(query, metadata, base_url, progress_signal, filename, is_running):
 
                     with open(f"{filename}.xml", 'a', encoding="utf-8") as f:
                         f.write(footer)
-                    break
+
+                    print("successfully written all records and footer.")
+                    progress_signal.emit(100)
+                    return True
 
                 if count % 50 == 0:
                     if not is_running():
@@ -248,6 +252,8 @@ def dnb_sru(query, metadata, base_url, progress_signal, filename, is_running):
             with open(f"{filename}.xml", 'w', encoding='utf-8') as f1:
                 f1.write(xmlplus)
 
+            return True
+
         else:
             print("Something went wrong.")
 
@@ -256,7 +262,7 @@ def dnb_sru(query, metadata, base_url, progress_signal, filename, is_running):
 
 class DNBSRUThread(QThread):
     progress_signal = pyqtSignal(int)
-    result_signal = pyqtSignal(object)
+    result_signal = pyqtSignal(bool)
 
     def __init__(self, query, metadata, base_url, filename):
         super().__init__()
@@ -268,7 +274,8 @@ class DNBSRUThread(QThread):
 
     def run(self):
         # Übergabe `is_running` Funktion an `dnb_sru`
-        dnb_sru(self.query, self.metadata, self.base_url, self.progress_signal, self.filename, self.is_running)
+        success = dnb_sru(self.query, self.metadata, self.base_url, self.progress_signal, self.filename, self.is_running)
+        self.result_signal.emit(success)
 
     def stop(self):
         self._is_running = False
@@ -276,25 +283,61 @@ class DNBSRUThread(QThread):
     def is_running(self):
         return self._is_running
 
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        self.logo_label.setGeometry(self.width() - 60, 10, 50, 50)
+
 
 class SRUQueryApp(QMainWindow):
     def __init__(self):
         super().__init__()
+        app = QApplication.instance()
+        font = app.font()
+        font.setFamily("Verdana")
+        app.setFont(font)
+
         self.setWindowTitle("SRU Query Tool")
-        self.setGeometry(100, 100, 600, 500)
+        self.setGeometry(100, 100, 750, 750)
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
         layout = QVBoxLayout(central_widget)
         layout.setSpacing(10)  # Abstand zwischen Widgets
         layout.setContentsMargins(20, 20, 20, 20)  # Ränder des Layouts
 
-        layout.addWidget(QLabel("""Bitte wählen Sie im folgenden einen Katalog und ein Metadatenformat aus, 
-        um die SRU-Schnittstelle der DNB abzufragen:"""))
-        layout.addWidget(QLabel(" "))
+        heading = QLabel("SRU Query Tool")
+        heading.setStyleSheet("""
+            font-size: 16pt;
+            font-weight: bold;
+            margin-bottom: 10px;
+        """)
+        heading.setAlignment(Qt.AlignCenter)
+        layout.addWidget(heading)
+        layout.addWidget(QLabel(" "))  # Empty label for spacing
+
+        # Create a QLabel with the text
+        intro_label = QLabel("Mit diesem Tool können Sie die SRU-Schnittstelle der Deutschen Nationalbibliothek "
+                            "abfragen und einen Metadatendump herunterladen. <br> Allgemeine Informationen "
+                            "zur SRU-Schnittstelle und den zur Verfügung stehenden Katalogen finden Sie unter "
+                            "<a href='https://www.dnb.de/sru'>www.dnb.de/sru</a>. Weiterführende Informationen "
+                            "zur Suche sowie Möglichkeiten zur Eingrenzung der Ergebnisse finden Sie unter "
+                            "<a href='https://www.dnb.de/expertensuche'>www.dnb.de/expertensuche</a>.")
+        #intro_label.setAlignment(Qt.AlignCenter)
+        intro_label.setWordWrap(True)  # wrap text
+        intro_label.setOpenExternalLinks(True)  # This allows the links to be clickable
+        layout.addWidget(intro_label)
+        layout.addWidget(QLabel(" "))  # Empty label for spacing
+
+        todo_label = QLabel("Wählen Sie einen Katalog und ein Metadatenformat aus und geben Sie Ihre Suchanfrage ein:")
+        todo_label.setAlignment(Qt.AlignCenter)
+        todo_label.setWordWrap(True) # wrap text
+
+        # Add the label to the layout
+        layout.addWidget(todo_label)
+        layout.addWidget(QLabel(" "))  # Empty label for spacing
 
         layout.addWidget(QLabel("Katalog:"))
         self.catalogue_combo = QComboBox()
-        self.catalogue_combo.addItems(["DNB Titeldaten", "GND (Normdaten)", "DMA (Deutsches Musikarchiv)",
+        self.catalogue_combo.addItems(["DNB (Titeldaten)", "GND (Normdaten)", "DMA (Deutsches Musikarchiv)",
                                        "ZDB (Zeitschriftendatenbank)", "Adressdaten (ISIL- und Siegelverzeichnis)"])
         layout.addWidget(self.catalogue_combo)
         self.catalogue_combo.currentIndexChanged.connect(self.update_metadata_formats)
@@ -318,13 +361,13 @@ class SRUQueryApp(QMainWindow):
         # Check Search Query Button
         self.check_button = QPushButton("Suchanfrage prüfen")
         self.check_button.clicked.connect(self.check_search_query)
-        self.check_button.setFixedSize(150, 40)
+        self.check_button.setFixedSize(180, 40)
 
         # Download XML Button
         self.download_button = QPushButton("Download XML")
         self.download_button.setEnabled(False)
         self.download_button.clicked.connect(self.get_xml)
-        self.download_button.setFixedSize(150, 40)
+        self.download_button.setFixedSize(180, 40)
         layout.addWidget(self.download_button)
 
         # Buttons zentrieren
@@ -364,7 +407,7 @@ class SRUQueryApp(QMainWindow):
         cancel_layout.addStretch()
         self.cancel_button = QPushButton("Abbrechen")
         self.cancel_button.setVisible(False)
-        self.cancel_button.setFixedSize(150, 40)
+        self.cancel_button.setFixedSize(180, 40)
         self.cancel_button.clicked.connect(self.stop_download)
         cancel_layout.addWidget(self.cancel_button)
         cancel_layout.addStretch()
@@ -377,9 +420,17 @@ class SRUQueryApp(QMainWindow):
         exit_layout = QHBoxLayout()
         exit_layout.addSpacerItem(QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum))
         self.exit_button = QPushButton("Schließen")
-        self.exit_button.setFixedSize(150, 40)
+        self.exit_button.setFixedSize(180, 40)
         self.exit_button.clicked.connect(self.close)
         exit_layout.addWidget(self.exit_button)
+
+        # Add logo:
+        self.logo_label = QLabel(self)
+        pixmap = QPixmap("dnb_RGB_125_81.gif")
+        self.logo_label.setPixmap(pixmap.scaled(80, 90, Qt.KeepAspectRatio, Qt.SmoothTransformation))
+        self.logo_label.setAlignment(Qt.AlignTop | Qt.AlignRight)
+        self.logo_label.setGeometry(self.width() - 110, 10, 90, 80)
+        self.logo_label.raise_()
 
         layout.addLayout(exit_layout)
 
@@ -389,7 +440,7 @@ class SRUQueryApp(QMainWindow):
     def update_metadata_formats(self):
         self.metadata_combo.clear()
         selected_catalogue = self.catalogue_combo.currentText()
-        if selected_catalogue == "DNB Titeldaten":
+        if selected_catalogue == "DNB (Titeldaten)":
             formats = ["MARC21-xml", "oai_dc", "RDFxml", "mods-xml"]
         elif selected_catalogue == "GND (Normdaten)":
             formats = ["MARC21-xml", "RDFxml"]
@@ -407,7 +458,7 @@ class SRUQueryApp(QMainWindow):
             metadata = self.metadata_combo.currentText()
             query = self.query_input.text()
 
-            if catalogue == "DNB Titeldaten":
+            if catalogue == "DNB (Titeldaten)":
                 cat_url = "https://services.dnb.de/sru/dnb"
             elif catalogue == "DMA (Deutsches Musikarchiv)":
                 cat_url = "https://services.dnb.de/sru/dnb.dma"
@@ -433,8 +484,12 @@ class SRUQueryApp(QMainWindow):
                     self.warning_label.setVisible(True)
                     self.download_button.setEnabled(False)
             else:
-                self.results_label.setText(
-                    "Bitte wählen Sie einen Katalog und ein Metadatenformat aus und geben Sie eine Suchanfrage ein.")
+                self.warning_label.setText(
+                    "Bitte wählen Sie zuerst einen Katalog und ein Metadatenformat aus und "
+                    "geben Sie eine Suchanfrage ein.")
+                self.warning_label.setAlignment(Qt.AlignCenter)
+                self.warning_label.setWordWrap(True)
+                self.warning_label.setVisible(True)
                 self.download_button.setEnabled(False)
         except Exception as e:
             self.results_label.setText(f"Ein Fehler ist aufgetreten: {str(e)}")
@@ -450,7 +505,7 @@ class SRUQueryApp(QMainWindow):
         self.status_label.setVisible(True)
         self.status_label.setText("Downloading...")
 
-        if catalogue == "DNB Titeldaten":
+        if catalogue == "DNB (Titeldaten)":
             cat_url = "https://services.dnb.de/sru/dnb"
         elif catalogue == "DMA (Deutsches Musikarchiv)":
             cat_url = "https://services.dnb.de/sru/dnb.dma"
@@ -476,10 +531,16 @@ class SRUQueryApp(QMainWindow):
     def update_progress(self, value):
         self.progress_bar.setValue(value)
 
-    def handle_result(self):
-        self.status_label.setText("Download erfolgreich!")
+    def handle_result(self, success):
+        if success:
+            self.status_label.setText("Download erfolgreich!")
+            self.download_button.setEnabled(True)
+            self.cancel_button.setVisible(False)
+        else:
+            self.status_label.setText("Download fehlgeschlagen.")
+
+        self.progress_bar.setVisible(False)
         self.download_button.setEnabled(True)
-        self.cancel_button.setEnabled(False)
 
     def stop_download(self):
         self.dnb_sru_thread.stop()
@@ -536,6 +597,7 @@ class SRUQueryApp(QMainWindow):
             QProgressBar::chunk {
                 background-color: #007BFF;
             }
+            
         """)
 
         # Exit-Button
